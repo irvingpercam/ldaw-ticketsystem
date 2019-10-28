76c089fc1153bc1e44786f6ef7c1b5f1a2605635
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2019-10-27 18:06:43 -0600

    Upload: Laravel project

--- ticketSite/routes/web.php
+++ ticketSite/routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+
